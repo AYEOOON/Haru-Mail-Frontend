f0fe256715277ad061bdd9725e29d9d3a81df123
AYEOOON <k93853471@gmail.com>
2025-05-08 12:55:29 +0900

    feat: 각 버튼에 따른 페이지 이동 경로 설정

--- client/src/pages/header/Header.tsx
+++ client/src/pages/header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header: React.FC = () => {
@@ -6,10 +7,10 @@ const Header: React.FC = () => {
         <header className="header">
             <div className="logo">하루 메일</div>
             <nav className="nav">
-                <a href="#">일기 목록</a>
-                <a href="#">일기 검색</a>
-                <a href="#">설정</a>
-                <a href="#">로그아웃</a>
+                <Link to="/list">일기 목록</Link>
+                <Link to="/search">일기 검색</Link>
+                <Link to="/setting">설정</Link>
+                <Link to="/logout">로그아웃</Link>
             </nav>
         </header>
     );
