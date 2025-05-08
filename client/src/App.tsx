e112c015fc13dda498f2e66446e753f0a8575570
AYEOOON <k93853471@gmail.com>
2025-05-08 12:52:19 +0900

    feat: 설정 페이지 라우팅 추가
    - /settings에 대한 Route 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/home/HomePage';  // 경로 수정
-import SuccessPage from './pages/success/SuccessPage';  // 성공 페이지 경로도 확인
+import HomePage from './pages/home/HomePage';  // 메인 페이지
+import TestPage from './pages/success/SuccessPage';  // 성공 페이지
+import ListPage from './pages/diary/list/ListPage';  // 사용자 일기 목록 페이지
+import SettingPage from './pages/setting/MailSettingPage';  // 메일 수신 빈도 설정 페이지
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/success" element={<SuccessPage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/setting" element={<SettingPage />} />
       </Routes>
     </Router>
   );
